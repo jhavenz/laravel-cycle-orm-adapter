21cfb5778e9b817b1a36825d53b407f8bc2fe51c
Jonathan Havens <jhavens@rcnmail.com>
2022-10-06 20:29:23 -0700

    add console commands to service provider

--- src/Bridge/Laravel/Providers/CycleServiceProvider.php
+++ src/Bridge/Laravel/Providers/CycleServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace WayOfDev\Cycle\Bridge\Laravel\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use WayOfDev\Cycle\Console\Commands;
 
 final class CycleServiceProvider extends ServiceProvider
 {
@@ -44,7 +45,16 @@ final class CycleServiceProvider extends ServiceProvider
     private function registerConsoleCommands(): void
     {
         $this->commands([
-            // ...
+            Commands\Database\ListCommand::class,
+            Commands\Database\TableCommand::class,
+            Commands\ORM\SyncCommand::class,
+            Commands\ORM\MigrateCommand::class,
+            Commands\ORM\UpdateCommand::class,
+            Commands\ORM\RenderCommand::class,
+            Commands\Migrations\MigrateCommand::class,
+            Commands\Migrations\InitCommand::class,
+            Commands\Migrations\RollbackCommand::class,
+            Commands\Migrations\ReplayCommand::class,
         ]);
     }
 }
